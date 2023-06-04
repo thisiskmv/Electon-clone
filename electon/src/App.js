a53507e494e0a99e47f7872cf5b22ab996c68b7c
meshashank99 <122826889+meshashank99@users.noreply.github.com>
2023-06-04 18:44:43 +0530

    Update App.js

--- electon/src/App.js
+++ electon/src/App.js
@@ -1,10 +1,36 @@
+import { useSelector } from 'react-redux';
+import ColorCheckbox from './Component/ColorCheckbox'
+import ProductDetail from './Component/ProductDetail';
+import Product from './Component/pages/Product';
 
-import './App.css';
-import Home from './components/Home';
+import Cart from './Component/pages/Cart';
+import Payment from './Component/pages/Payment';
+import Footer from './Component/Footer';
+
+import AllRoutes from "./Routes/AllRoutes";
+
 function App() {
+  // const { theme} = useSelector((store) => {
+  //   return store.themeReducer;
+  // });
+  // console.log(theme)
   return (
+
     <div className="App">
-      <Home/>
+      {/* <ProductDetail/> */}
+
+      {/* <ColorCheckbox/> */}
+      {/* <Product/> */}
+      <Cart />
+      <Payment />
+      <Footer />
+
+   
+     {/* <ProductDetail/> */}
+
+    <AllRoutes />
+    
+
     </div>
   );
 }
