75553b7bf33e707c693c05a142721c79a8481f74
abhi21121211 <111838547+abhi21121211@users.noreply.github.com>
2023-08-31 15:53:17 +0530

    all pages responsive

--- electon/src/Redux/Store.js
+++
@@ -1,7 +0,0 @@
-import {legacy_createStore as  createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducer';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
