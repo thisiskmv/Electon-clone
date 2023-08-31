94a810cba849e2d097f80e3edc85431fc59bf14d
abhi21121211 <111838547+abhi21121211@users.noreply.github.com>
2023-08-31 20:10:10 +0530

    all done

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -5,38 +5,38 @@ import "./Navbar.css"
 const Navbar = () => {
   return (
     <div>
-         <ul className='headerList'>
-            <div className='headerList1'>
-            <Link href="/productPage" color="white" underline="none">Browse categories
-            <ExpandMoreIcon/>
-            </Link>
-            </div>
-            <div className='headerList2'>
-            <Link href="/" color="black" underline="none">Home</Link>
-            <ExpandMoreIcon/>
-            </div>
-            <div className='headerList3'>
-            <Link href="#" color="black" underline="none">Catalog</Link>
-            <ExpandMoreIcon/>
-            </div>
-            <div className='headerList4'>
-            <Link href="#" color="black" underline="none">Blog</Link>
-            <ExpandMoreIcon/>
-            </div>
-            <div className='headerList5'>
-            <Link href="#" color="black" underline="none">Pages</Link>
-            <ExpandMoreIcon/>
-            </div>
-            <div className='headerList6'>
-            <Link href="#" color="black" underline="none">About us</Link>
-            </div>
-            
-            
-           
-            
-           
-            
-         </ul>
+      <ul className='headerList'>
+        <div className='headerList1'>
+          <Link href="/productPage" color="white" underline="none">Browse categories
+            <ExpandMoreIcon />
+          </Link>
+        </div>
+        <div className='headerList2'>
+          <Link href="/" color="black" underline="none">Home</Link>
+          <ExpandMoreIcon />
+        </div>
+        <div className='headerList3'>
+          <Link href="#" color="black" underline="none">Catalog</Link>
+          <ExpandMoreIcon />
+        </div>
+        <div className='headerList4'>
+          <Link href="#" color="black" underline="none">Blog</Link>
+          <ExpandMoreIcon />
+        </div>
+        <div className='headerList5'>
+          <Link href="#" color="black" underline="none">Pages</Link>
+          <ExpandMoreIcon />
+        </div>
+        <div className='headerList6'>
+          <Link href="#" color="black" underline="none">About us</Link>
+        </div>
+
+
+
+
+
+
+      </ul>
     </div>
   )
 }
